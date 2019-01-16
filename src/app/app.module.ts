66062c9e558c4e0e0ccb0327d58080f4fefdc6fb
Hans Li <hans@Hanss-MacBook-Pro.local>
2019-01-15 23:46:41 -0800

    create component, add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CreateTravelComponent } from './create-travel/create-travel.component';
+import { TravelHistoryComponent } from './travel-history/travel-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CreateTravelComponent,
+    TravelHistoryComponent
   ],
   imports: [
     BrowserModule
