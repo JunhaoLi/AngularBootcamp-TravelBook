b66ada85e7c156dbfcd60b34092516a919ffd0e1
Hans Li <lijunhao.aug@gmail.com>
2019-01-21 22:19:16 -0800

    auth state from home to navbar working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Travel Book";
+  isUserAuthorized = false;
+
+  onAuthorize(isUserAuthorized: boolean) {
+    console.log(isUserAuthorized);
+    this.isUserAuthorized = true;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @Output()isUserAuthorized: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSignupClick() {
+    this.isUserAuthorized.emit(true);
+  }
+
+  onSigninClick() {
+    this.isUserAuthorized.emit(true);
+  }
 }
